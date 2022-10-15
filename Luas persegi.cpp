51e929a885758073d284bbcc6974aadf62b47f22
allyaputri22 <114640843+allyaputri22@users.noreply.github.com>
2022-10-15 22:04:43 +0700

    Add files via upload

---
+++ Luas persegi.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    double luas,sisi;
+    cout << "Menghitung Luas Persegi" << endl;
+
+    cout << "Masukkan sisi  : ";
+    cin >> sisi;
+
+    //rumus luas persegi
+    luas = sisi * sisi;
+
+    cout << "Luas Persegi   = "<<luas;
+
+return 0;
+
+    }
